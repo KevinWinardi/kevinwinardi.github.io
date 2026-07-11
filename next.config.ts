b384937d5570f122947f741abc7ea27b70786b47
Kevin Winardi <kevinwinardi192005@gmail.com>
2026-07-11 22:40:25 +0700

    Remove next.config.mjs file and move configuration for mdx to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from 'next';
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     output: 'export',
     images: {
         unoptimized: true,
     },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+    extension: /\.(md|mdx)$/,
+    options: {
+        remarkPlugins: ['remark-frontmatter', 'remark-mdx-frontmatter'],
+    },
+});
+
+export default withMDX(nextConfig);
